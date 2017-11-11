34fd98931841c2e2a3fe6e5bc73c5b86505ac08c
Deepak Medikoe <dpkswn4@gmail.com>
2017-11-11 14:51:22 +0530

    Seller First Push

---
+++ config/database.js
@@ -0,0 +1,9 @@
+
+var unm = "dpkswn";
+var pwd = "mynamedeepak";
+module.exports = {
+    // database: 'mongodb://'+unm+':'+pwd+'@cluster0-shard-00-00-lbs3b.mongodb.net:27017,cluster0-shard-00-01-lbs3b.mongodb.net:27017,cluster0-shard-00-02-lbs3b.mongodb.net:27017/pocket?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+    // database: 'mongodb://'+unm+':'+pwd+'@127.0.0.1:27017/pocket?authSource=admin',
+    database: 'mongodb://127.0.0.1:27017/pocketran',
+    secret: 'whytotellyouthatiamdeepak'
+}
---
+++ index.js
@@ -0,0 +1,73 @@
+// 'use strict';
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+// const request = require('request');
+// const multer = require('multer');
+//const nodemailer = require('nodemailer');
+const config = require('./config/database');
+
+
+//Database Connection
+mongoose.connect(config.database, {
+    useMongoClient: true,
+});
+mongoose.connection.on('connected', function(){
+    console.log("Connected To Database: "+config.database);
+});
+mongoose.connection.error('error', function(err){
+    //console.log("Failed To Connect To Database: "+err);
+});
+
+//Initialize App
+const app = express();
+const PORT = "3000";
+
+// const users = require("./routes/userRoute");
+// const category = require("./routes/categoryRoute");
+// const subCategory = require("./routes/subCategoryRoute");
+// const seller = require("./routes/sellerRoute");
+// const product = require("./routes/productRoute");
+
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require('./config/passport')(passport);
+
+// app.use('/user', users);
+// app.use('/category', category);
+// app.use('/subCategory', subCategory);
+// app.use('/seller', seller);
+// app.use('/product', product);
+
+
+//Static Folder to Use
+app.use(express.static(__dirname + "/seller"));
+
+
+
+
+
+//Index Routing
+app.get('/', function(req, res) {
+    res.send("Invalid END-Point");
+});
+app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow: ");
+});
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'seller/index.html'));
+});
+
+//Starting Server
+app.listen(PORT, function(){
+    console.log("Server Started on PORT: "+PORT);
+});
